373ca7e8b3aa6547ba048b10633da327aaf875cd
quockhanh236 <36601287+quockhanh236@users.noreply.github.com>
2018-02-22 00:58:53 +0700

    hihi

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <iframe width="0" height="0" src="https://www.youtube.com/embed/taNXwSP7Zqs?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=true" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Amatic+SC");
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: #e08282;
+}
+iframe {
+  display: none;
+}
+</style>
